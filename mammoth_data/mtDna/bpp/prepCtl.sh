a7b20efbda64ddcad0c98ac8c76a07f61499116a
Anna Nagel <aanagel@ucdavis.edu>
2024-04-15 09:38:22 -0700

    Minor revisions for review

--- mammoth_data/mtDna/bpp/prepCtl.sh
+++ mammoth_data/mtDna/bpp/prepCtl.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 seed=1
-for rep in 1 2
+for rep in 1 2 3 4 
 do
 
 	mkdir -p $rep
-	sed "s/seed = 1/seed = ${seed}/g" mt.ctl >  $rep/mt.ctl
-	((seed=seed+1))
+	sed "s/seed = 1/seed = ${rep}/g" mt.ctl >  $rep/mt.ctl
 done
